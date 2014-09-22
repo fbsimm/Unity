a4d307ea18b5f1b8422c7ae4fd1af00c826d83e1
fbsimm <utilisateur@imm-850-3-07.priv.cjonquiere.qc.ca>
2014-09-22 09:41:00 -0400

    modification du GUI
    La grosseur des point et des vie change avec l’ecran

--- Assets/script/GUICamera.cs
+++ Assets/script/GUICamera.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class GUICamera : MonoBehaviour {
 	public GameObject player;
 	public static int score = 0;
+	public Texture2D coeur;
+
+	float virtualWidth = 600f;
+	float virtualHeight = 400f;
+
+	private Vector3 scale;
+	
+
 	// Use this for initialization
 	void Awake() {
 		//DontDestroyOnLoad(gameObject);
@@ -16,10 +24,23 @@ public class GUICamera : MonoBehaviour {
 	}
 
 	void OnGUI() {
+
+		scale.x = Screen.width / virtualWidth;
+		scale.y = Screen.height / virtualHeight;
+		scale.z = 1;
+
+		Matrix4x4 svMat = GUI.matrix;
+
+		GUI.matrix = Matrix4x4.Scale (scale);
+
 		GUI.Label(new Rect(10, 10, 100, 20), "Score :" + score);
 
 
 		GUI.Label(new Rect(40, 35, 100, 20), "X" +mouvement.chance);
+
+		GUI.Label(new Rect(5, 15, coeur.width, coeur.height), coeur);
+
+		GUI.matrix = svMat;
 	}
 
 }
