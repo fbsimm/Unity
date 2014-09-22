4d12c5a147e6ecebd5a3afe2ee47655a2b9e25f8
fbsimm <utilisateur@imm-850-3-06.priv.cjonquiere.qc.ca>
2014-09-22 10:25:24 -0400

    MÀJ
    - Ajout de commentaires dans le code Mouvement.cs
    - Modifié le système d’animation. J’ai ajouté un trigger « Run » (à
    finir)
    - J’ai modifié la force du saut. (À débugger)

--- Assets/script/mouvement.cs
+++ Assets/script/mouvement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class mouvement : MonoBehaviour {
 
+	//Variable généraux.
 	public GameObject player;
 	public float maxSpeed = 10f;
 	private bool grounded = false;
@@ -16,35 +17,40 @@ public class mouvement : MonoBehaviour {
 	private bool mort = false;
 	private bool depart = true;
 
+	//Variable pour pas écrire "Animator" à chaque fois, parce que nous les programmeurs on est vache.
 	Animator anim;
 
-	// Var pour le temps
+	// Var pour le temps.
 	double timer;
 	double timer_punch;
 	double timer_slide;
 	double timer_debut;
 
-	// Use this for initialization
+	// Initialiser les trucs.
 	void Start () {
 		anim = GetComponent<Animator>();
 
-		//Init pour le temps
+		//Init pour le temps.
 		timer = 0;
 		timer_punch = 0;
 		timer_slide = 0;
 		timer_debut = 0;
 	}
 	
-	// Appelé a chaques frame dans 1 sec
+	// Appelé a chaques frame que l'appareil génère.
 	void Update (){
+		//Variable pour pas écrire ça à chaque fois, parce que nous les programmeurs on est vache, encore. 
 		BoxCollider2D box = collider2D as BoxCollider2D;
 
+		//Au départ du jeu, 3 sec de temps mort.
 		if (depart) {
 			timer_debut += Time.deltaTime;
 			if(timer_debut >= 3){
 				depart = false;
 			}
 		}
+
+		//Quand le personnage meurt, 3 sec avant de retourner au début.
 		if(timer >= 3){
 			transform.position = new Vector3(-4.729806f,transform.position.y, transform.position.z);
 			mort = false;
@@ -53,48 +59,57 @@ public class mouvement : MonoBehaviour {
 			timer_debut = 0;
 			anim.Play("Idle");
 		}
-		//Punch
-		if(grounded && Input.GetKeyDown(KeyCode.Space)){
-			if(!punch){
-				CircleCollider2D circle = gameObject.AddComponent("CircleCollider2D") as CircleCollider2D;
-				circle.radius = 0.12f;
-				circle.center = new Vector2(0.36f, 0.12f);
-				circle.isTrigger = true;
+
+		if(!mort){
+			//Punch
+			if(grounded && Input.GetButtonDown("Punch")){
+				if(!punch){
+					CircleCollider2D circle = gameObject.AddComponent("CircleCollider2D") as CircleCollider2D;
+					circle.radius = 0.12f;
+					circle.center = new Vector2(0.36f, 0.12f);
+					circle.isTrigger = true;
+				}
+				anim.SetTrigger("Punch");
+				punch = true;
+				timer_punch = 0;
 			}
-			anim.SetTrigger("Punch");
-			punch = true;
-			timer_punch = 0;
-		}
-		//Saut
-		if(grounded && Input.GetKeyDown(KeyCode.UpArrow)){
-			anim.SetBool("grounded", false);
-			rigidbody2D.AddForce(new Vector2(0, jumpForce));
-		}
-		//Slide
-		if(grounded && Input.GetKeyDown(KeyCode.DownArrow)){
-			anim.SetTrigger("sliding");
-			box.size = new Vector2 (0.97f, 0.55f);
-			box.center = new Vector2 (0, -0.17f);
-			sliding = true;
-			timer_slide = 0;
+
+			//Saut
+			if(grounded && Input.GetButtonDown("Jump")){
+				anim.SetBool("grounded", false);
+				rigidbody2D.AddForce(new Vector2(0, jumpForce));
+			}
+
+			//Slide
+			if(grounded && Input.GetButtonDown("Slide")){
+				anim.SetTrigger("sliding");
+				box.size = new Vector2 (0.97f, 0.55f);
+				box.center = new Vector2 (0, -0.17f);
+				sliding = true;
+				timer_slide = 0;
+			}
 		}
 		//Trucs pour le temps
+
 		//Compteur pour le respawn
-		if(mort && !sliding && !punch)
+		if(mort)
 			timer += Time.deltaTime;
+
 		//Compteur pour le punch
 		if(punch){
 			timer_punch += Time.deltaTime;
-			if(timer_punch >= 1){
+			if(timer_punch >= 0.5f){
 				punch = false;
+				anim.SetTrigger("Run");
 				timer_punch = 0;
 				Destroy(GetComponent<CircleCollider2D>());
 			}
-			if(grounded && Input.GetKeyDown(KeyCode.UpArrow)){
+			if(grounded && Input.GetButtonDown("Slide") || grounded && Input.GetButtonDown("Jump")){
 				punch = false;
 				timer_punch = 0;
 			}
 		}
+
 		//Compteur pour le slide
 		if(sliding){
 			timer_slide += Time.deltaTime;
@@ -102,18 +117,21 @@ public class mouvement : MonoBehaviour {
 				box.size = new Vector2 (0.66f, 0.9f);
 				box.center = new Vector2 (0, 0);
 				sliding = false;
+				anim.SetTrigger("Run");
 				timer_slide = 0;
 			}
-			if(grounded && Input.GetKeyDown(KeyCode.UpArrow)){
+			if(grounded && Input.GetButtonDown("Punch") || grounded && Input.GetButtonDown("Jump")){
 				sliding = false;
 				timer_slide = 0;
 			}
 		}
 	}
-	/*Appelé a tout les 1/60 frame dans 1 sec*/
+	//Appelé a toutes les frames dans 1 sec.
 	void FixedUpdate(){
-
+		//C'est pour chercher la position du cercle qui vérifie si le joueur touche le sol.
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		//On met à jour les variables du "Animator".
 		anim.SetBool ("grounded", grounded);
 		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
@@ -129,7 +147,7 @@ public class mouvement : MonoBehaviour {
 
 	}
 
-	/*Aussitot que le joueur est en collision avec un Ennemi*/
+	//Aussitôt que le joueur est en collision avec un ennemi, on perd une vie.
 	void OnCollisionStay2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Ennemi") {
 			if(!mort){
@@ -141,6 +159,7 @@ public class mouvement : MonoBehaviour {
 		
 	}
 
+	//Si on frappe l'ennemie, on le détruit et on ajoute les points.
 	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.gameObject.tag == "Ennemi") {
 			Destroy(coll.gameObject);
@@ -152,4 +171,11 @@ public class mouvement : MonoBehaviour {
 		}
 	}
 
+	void OnTriggerStay2D(Collider2D coll){
+		if (coll.gameObject.tag == "Bureau") {
+			if(timer_slide >= 0.9f){
+				timer_slide = 0.9f;
+			}
+		}
+	}
 }
