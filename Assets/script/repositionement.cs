9baa610d8f24c8d93a7f7ecac3c8808f5571afa8
fbsimm <utilisateur@imm-850-3-04.priv.cjonquiere.qc.ca>
2014-11-24 11:02:01 -0500

    Background

--- Assets/script/repositionement.cs
+++ Assets/script/repositionement.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class repositionement : MonoBehaviour {
 	public Vector3 metToilacalisse;
+
 	// Use this for initialization
 	void Start () {
 		if (gameObject.CompareTag("Background_Ville1"))
 			metToilacalisse = new Vector3 (0, transform.position.y, transform.position.z);
+		/*else if (gameObject.CompareTag("Background_Ville2"))
+			metToilacalisse = new Vector3 (14, transform.position.y, transform.position.z);
+		else if (gameObject.CompareTag("Background_Ville3"))
+			metToilacalisse = new Vector3 (0, transform.position.y, transform.position.z);
+		else if (gameObject.CompareTag("Background_Ville4"))
+			metToilacalisse = new Vector3 (16, transform.position.y, transform.position.z);*/
 		else
 			metToilacalisse = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 	}
@@ -16,7 +23,7 @@ public class repositionement : MonoBehaviour {
 
 		if(mouvement.timer >= 3){
 
-			this.transform.position = metToilacalisse;
+			transform.position = metToilacalisse;
 		}
 	
 	}
