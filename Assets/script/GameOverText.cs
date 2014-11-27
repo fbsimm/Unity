4d2f0cb0390a6e1eb908d51b5bb887cf5ab3fff5
fbsimm <utilisateur@imm-850-3-06.priv.cjonquiere.qc.ca>
2014-11-27 11:49:43 -0500

    Try pour GameOver

---
+++ Assets/script/GameOverText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverText : MonoBehaviour {
+
+	private Vector2 pos;
+	public string texte;
+	Rect labelRect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		pos.x = Screen.width / 2f;
+		pos.y = Screen.height / 1.8f;
+		labelRect = GUILayoutUtility.GetRect (new GUIContent("Partie Terminée"), "label");
+
+		GUI.Label(labelRect, "Partie Terminée");
+	}
+}
