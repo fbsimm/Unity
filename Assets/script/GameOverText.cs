038156ff117353341fe52a15874bfddc789fde74
fbsimm <flyingbaconstudio@gmail.com>
2014-11-28 12:58:20 -0500

    Update GameOver

--- Assets/script/GameOverText.cs
+++ Assets/script/GameOverText.cs
@@ -2,26 +2,43 @@
 using System.Collections;
 
 public class GameOverText : MonoBehaviour {
-
-	private Vector2 pos;
+	
 	public string texte;
-	Rect labelRect;
+	public GUIStyle style;
+	Vector2 pos;
+	Color couleur;
+	bool depart = false;
+	//Rect labelRect;
 
 	// Use this for initialization
 	void Start () {
-	
+		couleur.r = 255;
+		couleur.g = 255;
+		couleur.b = 255;
+		couleur.a = 0;
+		style.normal.textColor = couleur;
+		Invoke ("dragon", 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!depart)
+						return;
+		couleur = style.normal.textColor;
+		if(couleur.a < 1)
+			couleur.a += 0.5f * Time.deltaTime;
+		style.normal.textColor = couleur;
+	}
+
+	void dragon(){
+		depart = true;
+
 	}
 
 	void OnGUI(){
-		pos.x = Screen.width / 2f;
-		pos.y = Screen.height / 1.8f;
-		labelRect = GUILayoutUtility.GetRect (new GUIContent("Partie Terminée"), "label");
 
-		GUI.Label(labelRect, "Partie Terminée");
+		pos = GUILayoutUtility.GetRect (new GUIContent (texte), "label").size;
+
+		GUI.Label (new Rect ((Screen.width/2)-(pos.x/2), (Screen.height/3)-(pos.y/2), pos.x, pos.y), texte, style);
 	}
 }
